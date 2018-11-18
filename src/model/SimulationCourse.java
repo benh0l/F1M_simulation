155d1ac60ff44bb76a30f0c7c300b293479b3ef3
Benoit Holzer <benoit.holzer8@etu.univ-lorraine.fr>
2018-11-18 13:15:31 +0100

    ;

--- src/model/SimulationCourse.java
+++ src/model/SimulationCourse.java
@@ -12,12 +12,13 @@ public class SimulationCourse {
 
     public int simulationPilot(Pilot p){
         int res = 0;
-        int random = new Random().nextInt(100);
-        if(random < 100 - (p.getLuck() + p.getTalent()) ){
-            int indice1 = (p.getBraking() + p.getCar().getChassis()) * track.getBraking();
-            int indice2 = (p.getCar().getMotor() + p.getOvertake()) * track.getSpeed();
-            int indice3 = (p.getTalent() + p.getCar().getAero()) * track.getDownforce();
-            int indice4 = (p.getTalent() + p.getStrategy()) * track.getDifficulty();
+        Random r = new Random();
+        int random = r.nextInt(100);
+        if(random <= 100 - ( (p.getLuck() + p.getTalent())/2 + track.getDifficulty()) ){
+            int indice1 = ((p.getBraking() + p.getCar().getChassis()) + (r.nextInt(2)-1) )* track.getBraking();
+            int indice2 = ((p.getCar().getMotor() + p.getOvertake()) + (r.nextInt(2)-1) )* track.getSpeed();
+            int indice3 = ((p.getTalent() + p.getCar().getAero()) + (r.nextInt(2)-1) ) * track.getDownforce();
+            int indice4 = ((p.getTalent() + p.getStrategy()) + (r.nextInt(2)-1) ) * track.getDifficulty();
             res = indice1+indice2+indice3+indice4;
         }
         return res;
